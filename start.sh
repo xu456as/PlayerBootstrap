c7df4ef39ef75ec9f27528033c8920dcaeb9d7f0
fangzheng.xu <fangzheng.xu@ele.me>
2018-04-15 19:00:11 +0800

    edit start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PROJECT_DIR=$(cd 'dirname $0';pwd)
+PROJECT_DIR=$(cd `dirname $0`;pwd)
 MAIN_CLASS=me.fetonxu.ServerMain
-CLASS_PATH="${PROJECT_DIR}/target/classes/"
+CLASS_PATH="${PROJECT_DIR}/target/classes/:${PROJECT_DIR}/target/dependency/*"
 java -server -classpath $CLASS_PATH $MAIN_CLASS
